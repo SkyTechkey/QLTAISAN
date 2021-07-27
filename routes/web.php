d7c90cf66e633e2e7d121947ed5e4cc7a915cd8e
unknown <admin@PNVD238.pn.com.vn>
2021-07-27 09:28:21 +0700

    chia layout cua Phat

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,49 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('index');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.Dashboard');
+})->name('dashboard');
+
+Route::get('index', function () {
+    return view('pages.Dashboard2');
+})->name('dashboard2');
+
+Route::get('index2', function () {
+    return view('pages.Dashboard3');
+})->name('dashboard3');
+
+Route::get('Widgets', function () {
+    return view('pages.Widgets');
+})->name('Widgets');
+
+Route::get('Tables/simple', function () {
+    return view('Tables.Simple_table');
+})->name('Simple_table');
+
+Route::get('Tables/data', function () {
+    return view('Tables.Data_table');
+})->name('Data_table');
+
+
+Route::get('Calendar', function () {
+    return view('pages.Calendar');
+})->name('Calendar');
+
+Route::get('Gallery', function () {
+    return view('pages.Gallery');
+})->name('Gallery');
+
+Route::get('Mail/inbox', function () {
+    return view('Mailbox.Inbox');
+})->name('Inbox');
+Route::get('Mail/compose', function () {
+    return view('Mailbox.Compose');
+})->name('Compose');
+Route::get('Mail/read', function () {
+    return view('Mailbox.Read');
+})->name('Read');
